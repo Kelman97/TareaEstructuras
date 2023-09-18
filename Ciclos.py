6c0c59c42ba77304bc1bbaca35b4e5a8074075d5
unknown <kelman.martinez0229@uhispano.ac.cr>
2023-09-18 12:47:26 -0600

    Estructura 1

---
+++ Ciclos.py
@@ -0,0 +1,9 @@
+Numero= 0
+while Numero<=10:
+    print(Numero)
+    Numero+=1
+if Numero%2 !=0:
+    print("Es un numero par",Numero)
+else:
+    print("Numero impar",Numero)
+    
---
+++ EstructurasDeControl.py
@@ -0,0 +1,33 @@
+""" Numero=3
+
+if Numero>6:
+    print("Hola mundo")
+    Numero= Numero + 1
+elif Numero== 3:
+    Numero= Numero + 3   
+elif Numero>7:
+    print("Python") 
+else:    
+    print(Numero)  
+
+print(Numero)         
+"""
+
+"""edad= int(input("Digite su edad: "))     
+if edad>=18:
+    print("Mayor de edad")
+else:
+    print("Es menor de edad")
+"""
+Salario= int(input("Digite su salario actual:"))
+if Salario<800:
+    print("no se paga iva")
+elif Salario>800 and Salario<1500:
+    print(Salario/10)
+elif Salario>1500 and Salario<2500:
+    print(Salario/20)
+elif Salario>=25002500:
+     print(Salario/25)
+
+
+
